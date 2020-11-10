dcbafd18929c18dcebea8f06f101c1f46576e517
Zulqoniun <muhammadqoni123@gmail.com>
2020-11-10 17:14:21 +0700

    Add project files.

---
+++ ServiceMtk-P1-120/IMatematika.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_120
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        [FaultContract(typeof(MathFault))]
+        /* output->*/
+        int Tambah(int a, int b); //method
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))]
+        int Kurang(int a, int b); //input
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))]
+        int Kali(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))]
+        int Bagi(int a, int b);
+
+        [OperationContract]
+        [FaultContract(typeof(MathFault))]
+        Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
+    }
+    [DataContract]
+
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
+    }
+
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x,y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+}
---
+++ ServiceMtk-P1-120/Matematika.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMtk_P1_120
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            try
+
+            {
+                return a / b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Kali(int a, int b)
+        {
+            try
+
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Kurang(int a, int b)
+        {
+            try
+
+            {
+                return a - b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Tambah(int a, int b)
+        {
+            try
+
+            {
+                return a + b;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+    }
+}
